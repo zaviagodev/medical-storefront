7510b71e713381dac6f63f6546c6b658d34f3570
umer2001 <umer2001.uf@gmail.com>
2025-06-05 09:48:03 +0700

    add: layout component for preview page

---
+++ src/app/preview/[...slug]/layout.tsx
@@ -0,0 +1,46 @@
+import { retrieveCart } from "@/lib/data/cart"
+import { retrieveCustomer } from "@/lib/data/customer"
+import { listCartFreeShippingPrices } from "@/lib/data/fulfillment"
+import { getBaseURL } from "@/lib/util/env"
+import CartMismatchBanner from "@/modules/layout/components/cart-mismatch-banner"
+import Footer from "@/modules/layout/templates/footer"
+import { NavigationHeader } from "@/modules/layout/templates/nav"
+import FreeShippingPriceNudge from "@/modules/shipping/components/free-shipping-price-nudge"
+import { StoreFreeShippingPrice } from "@/types/shipping-option/http"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  metadataBase: new URL(getBaseURL()),
+}
+
+export default async function PageLayout(props: { children: React.ReactNode }) {
+  const customer = await retrieveCustomer().catch(() => null)
+  const cart = await retrieveCart()
+  let freeShippingPrices: StoreFreeShippingPrice[] = []
+
+  if (cart) {
+    freeShippingPrices = await listCartFreeShippingPrices(cart.id)
+  }
+
+  return (
+    <>
+      <NavigationHeader />
+
+      {customer && cart && (
+        <CartMismatchBanner customer={customer} cart={cart} />
+      )}
+
+      {props.children}
+
+      <Footer />
+
+      {cart && freeShippingPrices && (
+        <FreeShippingPriceNudge
+          variant="popup"
+          cart={cart}
+          freeShippingPrices={freeShippingPrices}
+        />
+      )}
+    </>
+  )
+}
